319b075efcc71c9c5534ad1fe13ee4219cf84a6b
Anna Eklund <anna.eklund@yahoo.se>
2025-09-02 18:50:43 +0200

    Add random cocktail card on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
-import Card from "@/components/Card";
+import CocktailCard from "@/components/CocktailCard";
+import { ICocktailApiResponse } from "@/types";
 import styles from "./page.module.css";
+import { mapRawCocktailData } from "@/mapRawCocktailData";
 
-export default function Home() {
+async function fetchRandomCocktail() {
+  const response = await fetch(
+    "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+  );
+
+  const randomCocktailResponse: ICocktailApiResponse = await response.json();
+
+  return randomCocktailResponse.drinks[0];
+}
+
+export default async function Page() {
+  const rawRandomCocktail = await fetchRandomCocktail();
+  const randomCocktail = mapRawCocktailData(rawRandomCocktail);
+
   return (
     <div className={styles.page}>
-      <Card />
+      <CocktailCard cocktail={randomCocktail} />
     </div>
   );
 }
---
+++ src/components/CocktailCard.tsx
@@ -0,0 +1,27 @@
+import { ICocktail } from "@/types";
+import { createCocktailSlug } from "@/utils/urlHelpers";
+import Image from "next/image";
+import Link from "next/link";
+
+interface CocktailCardProps {
+  cocktail: ICocktail;
+}
+
+export default function CocktailCard({ cocktail }: CocktailCardProps) {
+  const slug = createCocktailSlug(cocktail.id, cocktail.name);
+
+  return (
+    <>
+      <article>
+        <Image
+          src={cocktail.thumbnail}
+          alt={cocktail.name}
+          width={200}
+          height={230}
+        />
+        <h2>{cocktail.name}</h2>
+        <Link href={`/cocktail/${slug}`}>See more</Link>
+      </article>
+    </>
+  );
+}
