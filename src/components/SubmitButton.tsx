44f6224d475db905ed99a023ab8a12a7e46eee31
Anna Eklund <anna.eklund@yahoo.se>
2025-09-09 11:46:06 +0200

    Add submit button loading indication

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -1,11 +1,23 @@
+import { Loader } from "lucide-react";
+
 interface SubmitButtonProps {
   text: string;
+  isLoading: boolean;
+  loadingText: string;
 }
 
-export default function SubmitButton({ text }: SubmitButtonProps) {
-  return (
-    <>
-      <button>{text}</button>
-    </>
-  );
+export default function SubmitButton({
+  text,
+  isLoading,
+  loadingText,
+}: SubmitButtonProps) {
+  if (isLoading) {
+    return (
+      <button type="submit" disabled>
+        {loadingText} <Loader />
+      </button>
+    );
+  }
+
+  return <button type="submit">{text}</button>;
 }
